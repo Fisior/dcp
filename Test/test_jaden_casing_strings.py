18ab882d2e6e79e17071fda6cda205608090146d
Kamil Cieślak <cieslakkam@gmail.com>
2021-08-16 12:18:47 +0200

    add new exercises

---
+++ Test/test_jaden_casing_strings.py
@@ -0,0 +1,5 @@
+from Exercises import jaden_casing_strings
+
+
+def test_jaden_casing_strings():
+    assert jaden_casing_strings("How can mirrors be real if our eyes aren't real") == "How Can Mirrors Be Real If Our Eyes Aren't Real"
